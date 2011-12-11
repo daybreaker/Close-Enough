00103ad5095fcd708dd9f01b4eba158ed09ccbc9
root <root@li110-237.members.linode.com>
2011-12-11 12:25:32 -0500

    fix migration

--- db/migrate/20111210214707_add_fields_to_locations.rb
+++ db/migrate/20111210214707_add_fields_to_locations.rb
@@ -1,12 +1,11 @@
 class AddFieldsToLocations < ActiveRecord::Migration
   def self.up
-    add_column :events, :band_name, :string
-    add_column :events, :location_id, :integer
-
+    add_column :locations, :status, :string
+    add_column :locations, :from_wwoz, :integer
   end
 
   def self.down
-    remove_column :events, :band_name
-    remove_column :events, :location_id
+    remove_column :locations, :status
+    remove_column :locations, :from_wwoz
   end
 end
