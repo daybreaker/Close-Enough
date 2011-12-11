78315ea57f50240faf08e586576ddd772b33dd0a
Brad Huber <daybreaker@gmail.com>
2011-12-10 22:10:14 -0600

    Some wwoz cleanuP

--- wwoz.rb
+++ wwoz.rb
@@ -23,45 +23,45 @@ class WWOZLivewire
       threads << Thread.new(url) do |page_url|
 
         venues_html = Nokogiri::HTML(open(page_url))
-	venues =  venues_html.css('div.view-content div.item-list ul li.views-row span.field-content a')
+	      venues =  venues_html.css('div.view-content div.item-list ul li.views-row span.field-content a')
 	      
-	venues.each do |venue|
-	  #puts venue.text + "\n"
-	  begin
-	    vurl = 'http://www.wwoz.org' + venue.attributes['href'].value
-	    venue_details_html = Nokogiri::HTML(open(vurl)).css('div.node')
-	    status = venue_details_html.css('.venue-status').text.downcase.include?("open") ? 'open' : 'closed'
-	    address = venue_details_html.css('.street-address').text + " " + venue_details_html.css('.locality').text + ', ' +  venue_details_html.css('.region').text
-	    map_matches = /q=(.*?)\+(.*?)\+/.match (venue_details_html.css('.location.map-link a').select{|x| x.text == "Google Maps" }.first.attributes['href'].value)
-	    lat, long = map_matches[1..2]
+	      venues.each do |venue|
+	      #puts venue.text + "\n"
+	        begin
+	          vurl = 'http://www.wwoz.org' + venue.attributes['href'].value
+	          venue_details_html = Nokogiri::HTML(open(vurl)).css('div.node')
+	          status = venue_details_html.css('.venue-status').text.downcase.include?("open") ? 'open' : 'closed'
+	          address = venue_details_html.css('.street-address').text + " " + venue_details_html.css('.locality').text + ', ' +  venue_details_html.css('.region').text
+	          map_matches = /q=(.*?)\+(.*?)\+/.match (venue_details_html.css('.location.map-link a').select{|x| x.text == "Google Maps" }.first.attributes['href'].value)
+	          lat, long = map_matches[1..2]
 		
-	    #places = @client.spots(lat, long, :name => venue.text )
+          	#places = @client.spots(lat, long, :name => venue.text )
 	
             unless Location.find_by_name(venue.text.strip)	
-	      l = Location.new({
-	        :vicinity => address,
-	        :lat => lat,
-	        :lng => long,
-	        :status => status,
-	        :from_wwoz => 1,
-	        :name => venue.text.strip
-	      })
+	            l = Location.new({
+	              :vicinity => address,
+	              :lat => lat,
+	              :lng => long,
+	              :status => status,
+	              :from_wwoz => 1,
+	              :name => venue.text.strip
+	            })
 		
-	      l.save
+  	          l.save
               puts "'#{l.name}' saved"	
-	    else
+	          else
               puts "'#{venue.text}' already in db"
             end
             sleep rand*2
           rescue Exception => e
             puts "#{venue} failed"
-          end      
-        end
-      end
+          end #end begin ... rescue     
+        end #end venues.each
+      end #end threads << Thread.do
 
-    end
+    end #end url.each
       
-   threads.each { |t| t.join }
-  end
+    threads.each { |t| t.join }
+  end #end scrape
   
 end
